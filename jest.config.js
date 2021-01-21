f509fa269729764ef0e452ce7e34ca58a4c61246
holyshared <holy.shared.design@gmail.com>
2021-01-21 12:25:15 +0900

    add build config

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,11 @@ module.exports = {
   transform: {
     "^.+\\.ts?$": "ts-jest"
   },
+  globals: {
+    "ts-jest": {
+      tsConfig: "test.tsconfig.json"
+    }
+  },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$",
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 };
